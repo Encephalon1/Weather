574caed022f25bc276a9fa3a476fc032a9034402
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2023-03-09 18:11:06 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Header from './header';
+import Maps from './Map.js'
+
+
+function App() {
+        return (
+            <Fragment>
+            <Maps />
+            </Fragment>            
+        );    
+}
+
+export default App;
