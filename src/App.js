257b8958b1d78f456a8f24b7562a715fbeb105f9
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2023-03-19 12:52:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Maps from './Map.js'
+import Weather from './Weather.js'
 
 
 function App() {
         return (
             <Fragment>
             <Maps />
+            <Weather />
             </Fragment>            
         );    
 }
